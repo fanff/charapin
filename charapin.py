f35b1cc5da35de58806c67d896793c22a87f4f0c
fanf <fanf@fanf>
2017-01-30 23:51:41 +0100

    better CLI

--- charapin.py
+++ charapin.py
@@ -4,7 +4,7 @@ import json
 import sox
 
 import charapinlib
-
+import shutil
 
 def main_json(args):
 
@@ -15,7 +15,7 @@ def main_json(args):
     
     subelem = []
     for channel in song["channels"]:
-        subelem.append( charapinlib.genChannel(channel,bpm=song["bpm"],repeat=3))
+        subelem.append( charapinlib.genChannel(channel,bpm=song["bpm"],repeat=args.repeat))
     
     
     finalName= None
@@ -30,9 +30,12 @@ def main_json(args):
     else:
         finalName= subelem[0]
 
-    if args.play:
+    if not args.noplay:
         charapinlib.play(finalName)
     
+    if args.write:
+        shutil.copy(finalName,args.write)
+
     # clean WORK
     charapinlib.cleanWORK()
 
@@ -40,8 +43,10 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     
-    parser.add_argument("songfile", help="songfile")
-    parser.add_argument("-p","--play", action="store_true",help="play song in the end")
+    parser.add_argument("songfile", help="songfile (json)")
+    parser.add_argument("-p","--noplay", action="store_true",default=False ,help="do not play song in the end.")
+    parser.add_argument("-r","--repeat", type=int ,default = 1 ,help="repeat song")
+    parser.add_argument("-w","--write", type=str ,help="write song as wav")
     #parser.add_argument("--pitchmf", type=int,help="play song in the end")
 
     charapinlib.makeFolders() 
